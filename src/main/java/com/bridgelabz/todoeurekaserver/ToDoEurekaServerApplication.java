ebdbdf5c862230dd986430dd8922ea0c89435567
Chaitra111 <111chaitra@gmail>
2018-08-14 19:00:22 +0530

    14/08/2018 EurekaServer commited

---
+++ src/main/java/com/bridgelabz/todoeurekaserver/ToDoEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.todoeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ToDoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoEurekaServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bridgelabz/todoeurekaserver/ToDoEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.todoeurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToDoEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
